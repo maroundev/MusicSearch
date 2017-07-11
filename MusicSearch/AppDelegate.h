261c347b937b0d1e8324cc6643d5da5eb927813b
Maroun Abi ramia <marounab27@gmail.com>
2017-07-10 18:16:04 -0700

    Initial Commit

---
+++ MusicSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicSearch
+//
+//  Created by Maroun Abi Ramia on 7/10/17.
+//  Copyright © 2017 Maroun Abi Ramia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicSearch
+//
+//  Created by Maroun Abi Ramia on 7/10/17.
+//  Copyright © 2017 Maroun Abi Ramia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
